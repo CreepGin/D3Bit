fb9d02d99b7c98455c54fc035e2ba7149fe4e7a9
CreepGin <st0724@gmail.com>
2012-08-17 17:15:30 -0400

    Fixes, imgur support, anonymous upload to D3Bit.com

--- D3BitGUI/GUI.cs
+++ D3BitGUI/GUI.cs
@@ -21,7 +21,7 @@ namespace D3BitGUI
 {
     public partial class GUI : Form
     {
-        private static string version = "1.1.0";
+        private static string version = "1.1.1";
 #if DEBUG
         private static bool debugMode = true;
 #else
